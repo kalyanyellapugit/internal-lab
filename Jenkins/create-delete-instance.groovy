596be03d47eb4453e4d85f2e7888a29088328aa7
kalyanyellapugit <157232913+kalyanyellapugit@users.noreply.github.com>
2024-01-23 20:13:14 +0530

    Update create-delete-instance.groovy

--- Jenkins/create-delete-instance.groovy
+++ Jenkins/create-delete-instance.groovy
@@ -28,7 +28,7 @@ pipeline {
 	stage('Create Instance') {
 	 steps {
     
-    sh 'gcloud compute instances $action $inst --zone=$zones
+    sh 'gcloud compute instances $action $inst --zone=$zones'
       
     }
     }
