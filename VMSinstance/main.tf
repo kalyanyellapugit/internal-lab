0979f15ecb471179712b406cdac28132f2d06fa3
kalyanyellapugit <157232913+kalyanyellapugit@users.noreply.github.com>
2024-02-19 23:32:57 +0530

    Update main.tf

--- VMSinstance/main.tf
+++ VMSinstance/main.tf
@@ -13,13 +13,7 @@ resource "google_compute_instance" "vm-instance1" {
   zone         = "us-central1-a"
   tags = ["web-app"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-10-buster-v20221102"
-    }
-  }
-
-  network_interface {
+ network_interface {
     network = "default"
 
   access_config {
